50936750e5bcb950c4a7e18af5a764e37ec36dfc
Thomas Richter <thomas.karl.richter@gmail.com>
2022-09-14 10:25:43 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+// create a loop with 10 times hello world
+for (var i = 0; i < 10; i++) {
+  console.log("Hello World");
+}
+
+// import solid client lib
+
+// write a function adds two numers if neg return   number in red div
+function add(a, b){
+  if (a < 0 or b < 0) {
+    return `<div style="color:red;">${a+b}</div>`;
+  }
+  return a + b;
+}
+  
+  
+  
+ function mycol (collection) {
+   let ret =[]
+   for (let name of collection) {
+     ret.push(name);
+   return ret;
+ } 
+  
+  
+  
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
